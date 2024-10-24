0591bf831bb0763286df2eb952018f62db27f9d9
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-10-24 10:55:11 -0400

    L10: Adding an Entity Framework DbContext

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+var services = builder.Services;
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("Default");
+services.AddDbContext<DataContext>(optionsAction => optionsAction.UseSqlite(connectionString));
 
 var app = builder.Build();
 
