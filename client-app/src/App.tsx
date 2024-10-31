cf02afac4b52a10a73cff004e7b46e5fe3dff1f3
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-10-31 14:12:39 -0400

    L28: Semantic UI React

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
 function App() {
   const url = "http://localhost:5000/api/activities";
@@ -13,12 +14,12 @@ function App() {
 
   return (
     <div>
-      <h1>Reactivities</h1>
-      <ul>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
         {activities.map((a: any) => (
-          <li key={a.id}>{a.title}</li>
+          <List.Item key={a.id}>{a.title}</List.Item>
         ))}
-      </ul>
+      </List>
     </div>
   );
 }
