afbc91087c7046034940b14e02c33cc7208a5610
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-11-04 11:40:11 -0500

    L34: Thin controllers in the API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,19 +1,14 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(IMediator mediator) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
-    private readonly IMediator _mediator = mediator;
-
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
-        => await _mediator.Send(new List.Query());
+        => await Mediator.Send(new List.Query());
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
