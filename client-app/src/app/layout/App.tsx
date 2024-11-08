7204627fd75e6a67fbd335f1f7021eb361032689
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-11-08 13:32:28 -0500

    L61: Setting up axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { Activity } from "../models/activity";
 import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,10 +13,9 @@ function App() {
   >(undefined);
   const [editMode, setEditMode] = useState(false);
 
-  const url = "http://localhost:5000/api/activities";
   useEffect(() => {
-    axios.get<Activity[]>(url).then((response) => {
-      setActivities(response.data);
+    agent.Activities.list().then((response) => {
+      setActivities(response);
     });
   }, []);
 
