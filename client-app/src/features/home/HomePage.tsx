745d8d6b25d60c9c10518904cd0f6df5bca212bd
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-11-14 10:38:32 -0500

    L80: Adding routes

---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Container } from "semantic-ui-react";
+
+export default function HomePage() {
+  return (
+    <Container style={{ margintop: "7em" }}>
+      <h1>Home page</h1>
+    </Container>
+  );
+}
