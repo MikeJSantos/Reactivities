1c9c735c0d3a61823b3920028b60718666461283
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-11-19 16:00:23 -0500

    L108: Prep for client-side error handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,16 +3,25 @@ import NavBar from "./NavBar";
 import { observer } from "mobx-react-lite";
 import { Outlet, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  if (useLocation().pathname === "/") return <HomePage />;
+  let child =
+    useLocation().pathname === "/" ? (
+      <HomePage />
+    ) : (
+      <>
+        <NavBar />
+        <Container style={{ marginTop: "7em" }}>
+          <Outlet />
+        </Container>
+      </>
+    );
 
   return (
     <>
-      <NavBar />
-      <Container style={{ marginTop: "7em" }}>
-        <Outlet />
-      </Container>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
+      {child}
     </>
   );
 }
