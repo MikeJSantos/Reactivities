7619d6f1217bf4ba3aecf0f6153667f06b22a318
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-11-22 14:19:10 -0500

    L132: Create user DTOs

---
+++ API/DTO/UserDTO.cs
@@ -0,0 +1,9 @@
+namespace API.DTO;
+
+public class UserDTO
+{
+    public string UserName { get; set; }
+    public string DisplayName { get; set; }
+    public string Token { get; set; }
+    public string Image { get; set; }
+}
