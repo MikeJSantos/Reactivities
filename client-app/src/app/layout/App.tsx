96e474e872ed303b08ce1138e8a4d96914690cea
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-12-02 09:03:14 -0500

    L152: Adding modals

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,6 +7,7 @@ import { ToastContainer } from "react-toastify";
 import { useStore } from "../stores/store";
 import { useEffect } from "react";
 import LoadingComponent from "./LoadingComponent";
+import ModalContainer from "../common/modals/ModalContainer";
 
 function App() {
   const {
@@ -34,6 +35,7 @@ function App() {
 
   return (
     <>
+      <ModalContainer />
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       {child}
     </>
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -2,11 +2,49 @@ import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import LoginForm from "../users/LoginForm";
 
 export default observer(function HomePage() {
   const {
-    userStore: { isLoggedIn },
+    userStore: { isLoggedIn, user },
+    modalStore: { openModal },
   } = useStore();
+
+  const welcomeBack = (
+    <>
+      <Header
+        as="h2"
+        inverted
+        content={`Welcome to back ${user?.displayName}`}
+      />
+      <Button as={Link} to={"/activities"} size="huge" inverted>
+        Go to Activities!
+      </Button>
+    </>
+  );
+  const loginOrRegister = (
+    <>
+      <Button
+        size="huge"
+        inverted
+        onClick={() => {
+          openModal(<LoginForm />);
+        }}
+      >
+        Login
+      </Button>
+      <Button
+        size="huge"
+        inverted
+        onClick={() => {
+          openModal(<h1>Register</h1>);
+        }}
+      >
+        Register
+      </Button>
+    </>
+  );
+
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container>
@@ -17,20 +55,9 @@ export default observer(function HomePage() {
             alt="logo"
             style={{ marginBottom: 12 }}
           />
+          Reactivities
         </Header>
-        {isLoggedIn ? (
-          <>
-            <Header as="h2" inverted content="Welcome to Reactivities" />
-          </>
-        ) : null}
-        <Button
-          as={Link}
-          to={isLoggedIn ? "/activities" : "/login"}
-          size="huge"
-          inverted
-        >
-          {isLoggedIn ? "Go to Activities" : "Login"}!
-        </Button>
+        {isLoggedIn ? welcomeBack : loginOrRegister}
       </Container>
     </Segment>
   );
