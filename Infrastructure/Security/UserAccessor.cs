dbf291e4da53db08ddf738a07d3535c59017d5f1
Michael Santos <8728510+MikeJSantos@users.noreply.github.com>
2024-12-02 12:36:13 -0500

    L158: Add Infrastructure project

---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,13 @@
+using System.Security.Claims;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Security;
+
+public class UserAccessor(IHttpContextAccessor httpContextAccessor) : IUserAccessor
+{
+    private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+    public string GetUsername()
+        => _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+}
